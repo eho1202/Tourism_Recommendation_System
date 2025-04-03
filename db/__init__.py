fa5eaab532bdee258edf6846b65647001b461e21
Ernestine Ho <ernestineho1202@gmail.com>
2025-04-03 18:44:43 -0400

    update init

--- db/__init__.py
+++ db/__init__.py
@@ -1,4 +1,4 @@
 from .recommender_db import RecommenderCommands
-from .location_db import LocationsCommands
+from .location_db import LocationCommands
 from .user_db import UserCommands
 from .connections import ConnectionManager
--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
-from .locations import Location
+from .locations import LocationModel
 from .recommendations import RatingModel, RecommendationsModel
 from .users import UserModel, LoginRequestModel, UserResponseModel, RegisterRequestModel
